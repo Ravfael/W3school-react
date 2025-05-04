f3cf8dd1719dc3e27b37b6feaad9e8b4819691e8
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-04 21:08:24 +0700

    tes push & commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 style={{ color: "blue" }}>Hello World</h1>
+      <p>hello everyone</p>
     </div>
   );
 }

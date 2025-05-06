dcc3e46f696b3332f7f7cbc959a22ba5abe78c93
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-06 10:18:32 +0700

    ES6 done

--- src/App.tsx
+++ src/App.tsx
@@ -10,3 +10,47 @@ function App() {
 }
 
 export default App;
+
+// Classes
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+
+  present() {
+    return "I have a " + this.brand;
+  }
+}
+
+class Model extends Car {
+  constructor(name, mod) {
+    super(name);
+    this.model = mod;
+  }
+
+  show() {
+    return this.present() + ", it is a " + this.model;
+  }
+}
+const myCar = new Model("Ford", "Mustang");
+myCar.show();
+
+// Arrow Function
+
+hello = function () {
+  return "hello worl";
+};
+
+hello = () => {
+  return "hello world";
+};
+
+hello = () => "hello world";
+
+hello = (val) => "hello" + val;
+
+// Map
+const myArray = ["apple", "banana", "orange"];
+const myList = myArray.map((item) => {
+  item;
+});

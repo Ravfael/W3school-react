cc7eb1f48cf0948d5f2e0501dc95a040a97e19b9
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-13 23:08:11 +0700

    props & events done

--- src/components/belajar-1.tsx
+++ src/components/belajar-1.tsx
@@ -1,1 +1,30 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+// props
+function Car(props) {
+  return <h2>im a {props.brand}</h2>;
+}
+
+function Garage() {
+  const carName = "Ford";
+  return (
+    <>
+      <h1>who lives in my garage?</h1>
+      <Car brand={carName} />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render();
+
+// events
+function Football() {
+  const shoot = () => {
+    alert("greet shoot");
+  };
+  return <button onClick={shoot}>take a shoot</button>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Football />);

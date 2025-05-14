8b0e81ef6019796d8b435f3e6b9fb9f235f82138
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-14 09:48:10 +0700

    conditional & lists are done

--- src/components/belajar-1.tsx
+++ src/components/belajar-1.tsx
@@ -28,3 +28,53 @@ function Football() {
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Football />);
+
+//conditional
+function MissedGoal() {
+  return <h1>MISSED</h1>;
+}
+function MadeGoal() {
+  return <h1>GOALL</h1>;
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal />;
+  }
+  return <MissedGoal />;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Goal isGoal={false} />);
+
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 && <h2>you have {cars.length} cars in your garage</h2>}
+    </>
+  );
+}
+const cars = ["ford", "bmw", "audi"];
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Garage cars={cars} />);
+
+// lists
+function Car(props) {
+  return <li>im a {props.brand}</li>;
+}
+
+function Garage() {
+  const cars = ["ford", "bmw", "audi"];
+  return (
+    <>
+      <h1>who lives in my garage</h1>
+      {cars.map((car) => (
+        <Car brand={car} />
+      ))}
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Garage />);

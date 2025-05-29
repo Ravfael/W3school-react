5f4e82ade79642d90d7ebcaf4d803e6d07efbdb2
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-29 22:07:31 +0700

    useReducer done

--- src/components/useRef.tsx
+++ src/components/useRef.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useReducer } from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
@@ -17,3 +17,55 @@ function App() {
 }
 const root = ReeactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+//useReducer
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    complete: false,
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "COMPLETE":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+function Todos() {
+  const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+  const handleComplete = (todo) => {
+    dispatch({ type: "COMPLETE", id: todo.id });
+  };
+
+  return (
+    <>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <label>
+            <input type="checkbox" checked={todo.complete} onChange={() => handleComplete(todo)} />
+            {todo.title}
+          </label>
+        </div>
+      ))}
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Todos />);

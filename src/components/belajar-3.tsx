fb8c5262dfdebf7216525439509bc5bf097606d6
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-29 10:20:25 +0700

    useEffect done

--- src/components/belajar-3.tsx
+++ src/components/belajar-3.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function FavoriteColor() {
@@ -46,3 +46,33 @@ function Car() {
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Car />);
+
+// useEffect
+function TImer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => +1);
+    }, 1000);
+  }, []);
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
+}

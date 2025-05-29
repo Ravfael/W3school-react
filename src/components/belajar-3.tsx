7f341662a0ebd236f75dc11c35246ad013e19a07
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-05-29 21:01:38 +0700

    useContext and useRef done

--- src/components/belajar-3.tsx
+++ src/components/belajar-3.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
 import ReactDOM from "react-dom";
 
 function FavoriteColor() {
---
+++ src/components/useRef.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <>
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+    </>
+  );
+}
+const root = ReeactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
